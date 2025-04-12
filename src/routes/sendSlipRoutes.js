77e3b97c4843f76667ff6f0f58e67c347dda7373
Jeammm <87964996+Jeammm@users.noreply.github.com>
2025-04-12 20:38:53 +0700

    confuse af

--- src/routes/sendSlipRoutes.js
+++ src/routes/sendSlipRoutes.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const generateSlip = require("../genSlipImage");
 const sendSlipImage = require("../sendSlipImage");
+const client = require('../lineClient');
+
 
 async function handleSendSlip(userId, rentData) {
   const slipPath = await generateSlip(rentData);
